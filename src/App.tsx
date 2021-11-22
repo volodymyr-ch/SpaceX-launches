5a9e49a14efaf71421fd66e5971e2aae0e96b5a3
Volodymyr Cherkavskyi <volodymyr.cherkavskyi@techmagic.co>
2021-11-22 14:55:15 +0200

    FIX: error during test on pre-push

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,9 @@
-import React, { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import React from 'react';
 
-import { getLaunchDataRequest } from 'store/actions';
-import { RootState } from 'store/types';
+const App = () => (
+  <div>
+    <h1>SpaceX launch</h1>
+  </div>
+);
 
-const App = () => {
-  const dispatch = useDispatch();
-  const { data: launchData } = useSelector(
-    ({ launches }: RootState) => launches,
-  );
-
-  useEffect(() => {
-    dispatch(getLaunchDataRequest(30));
-  }, []);
-
-  useEffect(() => {
-    console.log('data', launchData);
-  }, [launchData]);
-
-  return (
-    <div>
-      <h1>SpaceX launch</h1>
-    </div>
-  );
-};
 export default App;
