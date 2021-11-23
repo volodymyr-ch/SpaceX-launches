193f6e84123ff126ae19d851439be063e00a8bb1
Volodymyr Cherkavskyi <volodymyr.cherkavskyi@techmagic.co>
2021-11-23 16:06:29 +0200

    FIX: problem with deploy

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,15 +1,12 @@
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 // eslint-disable-next-line import/no-extraneous-dependencies
-import { composeWithDevTools } from 'redux-devtools-extension';
+import { composeWithDevTools } from 'redux-devtools-extension/logOnlyInProduction';
 
 import reducers from 'store/reducers';
 import rootSaga from 'store/sagas';
 
-const isProd = process.env.NODE_ENV === 'production';
-const composeEnhancers = isProd
-  ? compose
-  : composeWithDevTools({ trace: true, traceLimit: 25 });
+const composeEnhancers = composeWithDevTools({ trace: true, traceLimit: 25 });
 
 const sagaMiddleware = createSagaMiddleware();
 const middlewares = [sagaMiddleware];
